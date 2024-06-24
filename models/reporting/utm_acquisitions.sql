c795a9baa75fbfc89225c75feac7f77703805e7e
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-06-24 17:07:46 -0400

    Update utm_acquisitions.sql

--- models/reporting/utm_acquisitions.sql
+++ models/reporting/utm_acquisitions.sql
@@ -61,32 +61,32 @@ WITH previous_orders as
     GROUP BY 1,2,5,6,7,8),
     
     current_orders as 
-    (SELECT DATE_TRUNC('day',order_created_date_est::date) as date, 'day' as date_granularity, COUNT(*) as utm_acquisitions, 0 as utm_first_orders, utm_campaign,utm_content, utm_medium, utm_source
-    FROM {{ source('gsheet_raw','utm_acquisitions_2024') }}
+    (SELECT DATE_TRUNC('day',order_created_date_est::date) as date, 'day' as date_granularity, COUNT(order_oms_order_id) as utm_acquisitions, COUNT(CASE WHEN customer_type ~* 'First Order' THEN order_oms_order_id END) as utm_first_orders, utm_campaign,utm_content, utm_medium, utm_source
+    FROM {{ source('gsheet_raw','utm_acquisitions_2024_with_utm_content') }}
     GROUP BY 1,2,5,6,7,8
     
     UNION ALL
     
-    SELECT DATE_TRUNC('week',order_created_date_est::date) as date, 'week' as date_granularity, COUNT(*) as utm_acquisitions, 0 as utm_first_orders, utm_campaign,utm_content, utm_medium, utm_source
-    FROM {{ source('gsheet_raw','utm_acquisitions_2024') }}
+    SELECT DATE_TRUNC('week',order_created_date_est::date) as date, 'week' as date_granularity, COUNT(order_oms_order_id) as utm_acquisitions, COUNT(CASE WHEN customer_type ~* 'First Order' THEN order_oms_order_id END) as utm_first_orders, utm_campaign,utm_content, utm_medium, utm_source
+    FROM {{ source('gsheet_raw','utm_acquisitions_2024_with_utm_content') }}
     GROUP BY 1,2,5,6,7,8
     
     UNION ALL
     
-    SELECT DATE_TRUNC('month',order_created_date_est::date) as date, 'month' as date_granularity, COUNT(*) as utm_acquisitions, 0 as utm_first_orders, utm_campaign,utm_content, utm_medium, utm_source
-    FROM {{ source('gsheet_raw','utm_acquisitions_2024') }}
+    SELECT DATE_TRUNC('month',order_created_date_est::date) as date, 'month' as date_granularity, COUNT(order_oms_order_id) as utm_acquisitions, COUNT(CASE WHEN customer_type ~* 'First Order' THEN order_oms_order_id END) as utm_first_orders, utm_campaign,utm_content, utm_medium, utm_source
+    FROM {{ source('gsheet_raw','utm_acquisitions_2024_with_utm_content') }}
     GROUP BY 1,2,5,6,7,8
     
     UNION ALL
     
-    SELECT DATE_TRUNC('quarter',order_created_date_est::date) as date, 'quarter' as date_granularity, COUNT(*) as utm_acquisitions, 0 as utm_first_orders, utm_campaign,utm_content, utm_medium, utm_source
-    FROM {{ source('gsheet_raw','utm_acquisitions_2024') }}
+    SELECT DATE_TRUNC('quarter',order_created_date_est::date) as date, 'quarter' as date_granularity, COUNT(order_oms_order_id) as utm_acquisitions, COUNT(CASE WHEN customer_type ~* 'First Order' THEN order_oms_order_id END) as utm_first_orders, utm_campaign,utm_content, utm_medium, utm_source
+    FROM {{ source('gsheet_raw','utm_acquisitions_2024_with_utm_content') }}
     GROUP BY 1,2,5,6,7,8
     
     UNION ALL
     
-    SELECT DATE_TRUNC('year',order_created_date_est::date) as date, 'year' as date_granularity, COUNT(*) as utm_acquisitions, 0 as utm_first_orders, utm_campaign, utm_content, utm_medium, utm_source
-    FROM {{ source('gsheet_raw','utm_acquisitions_2024') }}
+    SELECT DATE_TRUNC('year',order_created_date_est::date) as date, 'year' as date_granularity, COUNT(order_oms_order_id) as utm_acquisitions, COUNT(CASE WHEN customer_type ~* 'First Order' THEN order_oms_order_id END) as utm_first_orders, utm_campaign,utm_content, utm_medium, utm_source
+    FROM {{ source('gsheet_raw','utm_acquisitions_2024_with_utm_content') }}
     GROUP BY 1,2,5,6,7,8)
     
     SELECT * FROM previous_orders
